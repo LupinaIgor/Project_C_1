403c697197d92ec2b81d86af70a5203c55096f86
Игорь Лупина <igorlupina222@gmail.com>
2023-10-29 18:58:03 +0200

    \create class Drib

--- Project1/Source1.cpp
+++ Project1/Source1.cpp
@@ -1,5 +1,50 @@
 #include <iostream>
 using namespace std;
+class Drib {
+private:
+    int chiselnik;
+    int znamennik;
+
+public:
+    // Конструктор за замовчуванням
+    Drib() {
+        chiselnik = 0;
+        znamennik = 1;
+    }
+
+    // Конструктор з параметрами 
+    Drib(int chis, int znam)
+    {
+        chiselnik = chis;
+        znamennik = znam;
+    }
+    // Метод доступу до чисельника та знаменника
+    int GetChiselnik() {
+        return chiselnik;
+    }
+
+    int GetZnamennik() {
+        return znamennik;
+    }
+
+    void SetChiselnik(int chis) {
+        chiselnik = chis;
+    }
+
+    void SetZnamennik(int znam) {
+        if (znam != 0) {
+            znamennik = znam;
+        }
+        else {
+            cout << "Помилка: Знаменник не може бути рівним нулю" << endl;
+        }
+    }
+
+    // Функція для виводу дробу
+    void Print() {
+        cout << chiselnik << "/" << znamennik;
+    }
+};
 
 
 
